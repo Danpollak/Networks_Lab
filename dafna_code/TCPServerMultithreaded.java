2eac4f9ddd11c181ff897a776663f122464b3968
dafna1228 <dafna1228@gmail.com>
2018-12-13 21:20:15 +0200

    dafna - wrote my part of the code (thread handleing and shit)

--- dafna_code/TCPServerMultithreaded.java
+++ dafna_code/TCPServerMultithreaded.java
@@ -1,3 +1,4 @@
+package dafna_code;
 import java.io.*;
 import java.net.*;
 import java.util.concurrent.ExecutorService;
@@ -8,49 +9,60 @@ class EchoRunnable implements Runnable {
 
     public EchoRunnable(Socket clientSocket) {
         this.clientSocket = clientSocket;
-//        try {
-//            this.clientSocket.setSoTimeout(500);
-//        } catch (SocketException e) {
-//        }
+        try {
+            this.clientSocket.setSoTimeout(500);
+        } catch (SocketException e) {
+            System.err.println("Connection error: worker thread got timeout - " + e);
+        }
 
     }
 
     @Override
     public void run() {
+        // this is some code from an older tirgul, it just prints what it received from the client
         String clientSentence;
-        String capitalizedSentence;
+
         try (
                 BufferedReader inFromClient =
                         new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream())
         ) {
             clientSentence = inFromClient.readLine();  //read
-            // System.out.println("Received: " + clientSentence);
-            if (clientSentence != null) {
-                capitalizedSentence = clientSentence.toUpperCase() + '\n';
-                outToClient.writeBytes(capitalizedSentence);  // write
-            }
+            System.out.println("Received: " + clientSentence);
+//            if (clientSentence != null) {
+//                capitalizedSentence = clientSentence.toUpperCase() + '\n';
+//                outToClient.writeBytes(capitalizedSentence);  // write to client
+//            }
+
         } catch (IOException e) {
             System.err.println(e.getMessage());
         } finally {
             try {
                 this.clientSocket.close();
-            } catch (IOException e) {}
+            } catch (IOException e) {
+            }
         }
     }
 }
 
 class TCPServerMultithreaded {
     public static void main(String argv[]) throws Exception {
-        ServerSocket welcomeSocket = new ServerSocket(6789);  // bind + listen
+        // listen to port 8080 for new clients
+        ServerSocket proxySocket = new ServerSocket(8080);
+        // create a thread pool with 20 threads
         ExecutorService executor = Executors.newFixedThreadPool(20);
-
-        while (true) {
-            Socket clientSocket = welcomeSocket.accept();
-            Runnable worker = new EchoRunnable(clientSocket);
-
-            executor.execute(worker);
+        try {
+            while (true) {
+                // listen to client connections
+                Socket clientSocket = proxySocket.accept();
+                Runnable worker = new EchoRunnable(clientSocket);
+                System.out.println("running new thread worker");
+                executor.execute(worker);
+            }
+        } catch (Exception e) {
+            System.err.println("Connection error: couldn't assign threads to clients - got error " + e);
+            executor.shutdown();
         }
-        // executor.shutdown();
+
     }
 }
