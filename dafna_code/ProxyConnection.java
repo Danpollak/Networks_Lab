8217327510c75690028c8a818218d534c2c89841
Dan <dandan888@gmail.com>
2018-12-17 14:03:11 +0200

    Created lots of threads

---
+++ dafna_code/ProxyConnection.java
@@ -0,0 +1,5 @@
+package dafna_code;
+
+public class ProxyConnection implements Runnable {
+    
+}
--- dafna_code/TCPServerMultithreaded.java
+++ dafna_code/TCPServerMultithreaded.java
@@ -4,47 +4,6 @@ import java.net.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-class ClientThread implements Runnable {
-    private Socket clientSocket = null;
-    private BufferedReader readFromClient;
-    private DataOutputStream writeToClient;
-
-    public ClientThread(Socket clientSocket, BufferedReader readFromClient, DataOutputStream writeToClient) {
-        this.clientSocket = clientSocket;
-        this.readFromClient = readFromClient;
-        this.writeToClient = writeToClient;
-        try {
-            this.clientSocket.setSoTimeout(500);
-        } catch (SocketException e) {
-            System.err.println("Connection error: worker thread got timeout - " + e);
-        }
-
-
-    }
-
-    @Override
-    public void run() {
-        // this is some code from an older tirgul, it just prints what it received from the client
-        String clientSentence;
-
-         try {
-            clientSentence = readFromClient.readLine();  //read
-            System.out.println("Received: " + clientSentence);
-            if (clientSentence != null) {
-                writeToClient.writeBytes(clientSentence.toUpperCase() + '\n');  // write to client
-            }
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        } finally {
-            try {
-                this.clientSocket.close();
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
-            }
-        }
-    }
-}
-
 class TCPServerMultithreaded {
     public static void main(String argv[]) throws Exception {
         // listen to port 8080 for new clients
@@ -55,16 +14,14 @@ class TCPServerMultithreaded {
             while (true) {
                 // there is currently no functionality for the dest host part- this program currently reads the
                 // request from the client, prints it in the server, and writes it bach to the client, capitalized.
-
-                // listen to client connections
                 Socket clientSocket = proxySocket.accept();
+                // listen to client connections
                 // open data streams to read and write to client
-                BufferedReader inFromClient =
-                            new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedInputStream inFromClient = new BufferedInputStream(clientSocket.getInputStream());
+                BufferedOutputStream outToClient = new BufferedOutputStream(clientSocket.getOutputStream());
 
                 // init threads
-                Runnable clientThread = new ClientThread(clientSocket, inFromClient, outToClient);
+                Runnable clientThread = new ClientThread(clientSocket, inFromClient, outToClient, executor);
 
                 System.out.println("running new thread worker");
                 // run threads
